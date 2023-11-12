1476772796125bd82a102863cee037e7b8ad0122
caresle <carabesle@gmail.com>
2023-11-12 17:45:35 -0600

    Add readme

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Helpers\ApiResponses;
-use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
